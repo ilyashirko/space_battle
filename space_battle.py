1ac2da880dc773ecb46cd3e3ed780772b187b0c4
ilyashirko <ilyashirko@gmail.com>
2022-08-13 17:13:53 +0300

    removed copy_paste

--- space_battle.py
+++ space_battle.py
@@ -7,23 +7,24 @@ import time
 from curses_tools import draw_frame, get_frame_size, read_controls
 
 
+async def stars_awaiting():
+    for _ in range(random.randint(1, 5)):
+        await asyncio.sleep(0)
+
+
 async def blink(canvas, row, column, symbol='*'):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        for _ in range(random.randint(1, 5)):
-            await asyncio.sleep(0)
+        await stars_awaiting()
 
         canvas.addstr(row, column, symbol)
-        for _ in range(random.randint(1, 5)):
-            await asyncio.sleep(0)
+        await stars_awaiting()
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        for _ in range(random.randint(1, 5)):
-            await asyncio.sleep(0)
+        await stars_awaiting()
 
         canvas.addstr(row, column, symbol)
-        for _ in range(random.randint(1, 5)):
-            await asyncio.sleep(0)
+        await stars_awaiting()
 
 
 async def fire(canvas,
